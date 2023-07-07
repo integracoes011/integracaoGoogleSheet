2d007230cc19cde2c282aae607af94ef6ee812a4
gusta <gustavo024dias@gmail.com>
2023-07-06 21:28:15 -0300

    teste

--- index.py
+++ index.py
@@ -10,33 +10,34 @@ def new_order():
     msg = "ok"
 
     payload = request.get_json()
+    return jsonify(payload)
 
-    if not (data_order := payload.get("xml_order")):
-        code = 400
-        msg = "datas not found"
-    else:
-        root = ET.Element("pedido")
-
-        for key, value in data_order.items():
-            ET.SubElement(root, key).text = value
-
-        # msg = '<?xml version="1.0" encoding="UTF-8"?>{}'.format(
-        #     ET.tostring(
-        #         ET.ElementTree(root).getroot(),
-        #         encoding="utf-8",
-        #         method="xml").decode()
-        # )
-
-        msg = ET.tostring(
-                ET.ElementTree(root).getroot(),
-                encoding="utf-8",
-                method="xml").decode()
-
-    return make_response(
-        jsonify({
-            "msg": msg
-        }), code
-    )
+    # if not (data_order := payload.get("xml_order")):
+    #     code = 400
+    #     msg = "datas not found"
+    # else:
+    #     root = ET.Element("pedido")
+    #
+    #     for key, value in data_order.items():
+    #         ET.SubElement(root, key).text = value
+    #
+    #     # msg = '<?xml version="1.0" encoding="UTF-8"?>{}'.format(
+    #     #     ET.tostring(
+    #     #         ET.ElementTree(root).getroot(),
+    #     #         encoding="utf-8",
+    #     #         method="xml").decode()
+    #     # )
+    #
+    #     msg = ET.tostring(
+    #             ET.ElementTree(root).getroot(),
+    #             encoding="utf-8",
+    #             method="xml").decode()
+    #
+    # return make_response(
+    #     jsonify({
+    #         "msg": msg
+    #     }), code
+    # )
 
 
 if __name__ == "__main__":
