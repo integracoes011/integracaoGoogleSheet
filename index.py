0b784b166bbc040513072cc583b0db815352dc5f
gusta <gustavo024dias@gmail.com>
2023-07-11 09:47:46 -0300

    test args

--- index.py
+++ index.py
@@ -122,37 +122,39 @@ def getprodutos():
 
 @app.route("/callback")
 def callback():
-    payload = request.args
-    code = payload.get("code")
-
-    payload = json.dumps({
-        "grant_type": "authorization_code",
-        "code": code
-    })
-    headers = {
-        'Authorization': 'Basic ' + config('BASIC_AUTHENTICATION'),
-        'Content-Type': 'application/json',
-        'Cookie': 'PHPSESSID=f4aa8gc0a6kr70ag2qfbi8iu1k'
-    }
-    response = requests.request("POST", f"{BASE_URL}oauth/token", headers=headers, data=payload)
-    if not col_bling.find_one({"_id": 0}):
-        col_bling.insert_one(
-            {
-                "_id": 0,
-                "token": response.json()["access_token"]
-            }
-        )
-    else:
-        col_bling.update_one(
-            {"_id": 0},
-            {"$set": {"token": response.json()["access_token"]}}
-        )
-    return jsonify(
-        {
-            "msg": "token gerado com sucesso!",
-            "token": response.json()["access_token"]
-        }
-    )
+    args = request.args
+    return  args
+    # payload = request.args
+    # code = payload.get("code")
+    #
+    # payload = json.dumps({
+    #     "grant_type": "authorization_code",
+    #     "code": code
+    # })
+    # headers = {
+    #     'Authorization': 'Basic ' + config('BASIC_AUTHENTICATION'),
+    #     'Content-Type': 'application/json',
+    #     'Cookie': 'PHPSESSID=f4aa8gc0a6kr70ag2qfbi8iu1k'
+    # }
+    # response = requests.request("POST", f"{BASE_URL}oauth/token", headers=headers, data=payload)
+    # if not col_bling.find_one({"_id": 0}):
+    #     col_bling.insert_one(
+    #         {
+    #             "_id": 0,
+    #             "token": response.json()["access_token"]
+    #         }
+    #     )
+    # else:
+    #     col_bling.update_one(
+    #         {"_id": 0},
+    #         {"$set": {"token": response.json()["access_token"]}}
+    #     )
+    # return jsonify(
+    #     {
+    #         "msg": "token gerado com sucesso!",
+    #         "token": response.json()["access_token"]
+    #     }
+    # )
 
 
 if __name__ == "__main__":
